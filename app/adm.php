3ac9ee03715fda6b38504b664617dc5b6b39f1df
Ian Zaque Santos <ianzaque.uefs@gmail.com>
2020-02-29 00:53:45 -0300

    Tela de listagem de aluno e FAQ
    Alterações pequenas tb nas telas dashboard e boss

--- app/adm.php
+++ app/adm.php
@@ -17,7 +17,7 @@ c.nome as nome_curso, i.nome as nome_intituicao, end_i.estado as estado_i, end_i
 
 
 <body class="bg-light">
-    <title>Dashboard - ADM</title>
+    <title>Página do Administrador</title>
     <nav class="navbar navbar-expand-lg navbar-light bg-dark text-white">
         <a class="navbar-brand text-white" href="dashboard.php">
             <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
@@ -29,20 +29,21 @@ c.nome as nome_curso, i.nome as nome_intituicao, end_i.estado as estado_i, end_i
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <div class="navbar-nav">
-                <a id="nav-first" class="nav-item nav-link text-light" href="dashboard.php">Início <span
+                <a id="nav-first" class="nav-item nav-link text-light btn" href="dashboard.php">Início <span
                         class="sr-only">(current)</span></a>
                 <a class="nav-item nav-link text-light btn" href="cadastrar_aluno.php">Cadastramento do Aluno</a>
-                <a class="nav-item nav-link text-light btn" href="listar_aluno.php">Listagem de Alunos</a>
+                <a class="nav-item nav-link text-light btn" href="listar-aluno.php">Listagem de Alunos</a>
                 <a class="nav-item nav-link text-light text-white active btn btn-secondary border-20"
                     href="adm.php">Página do Administrador</a>
                 <a class="nav-item ml-3 nav-link text-light btn border-20" href="boss.php">Boss</a>
+                <a class="nav-item ml-3 nav-link text-light btn border-20" href="faq.php">Perguntas Frequentes</a>
                 <a class="nav-item nav-link text-light ml-4 text-dark btn btn-light border-10" href="exit.php">Sair</a>
             </div>
         </div>
     </nav>
     <div class="corpo container-fluid p-5">
         <div class="container-fluid bg-white border-20 p-0">
-            <p class="h2">PAGINA DO ADM</p>
+            <p class="h2">Página do Administrador</p>
         </div>
     </div>
     <!-- CONTEUDO -->
